42bf2b863eeb62eecdec35c1cb20baf6476ee200
Fling <128939559+FlingisenFlong@users.noreply.github.com>
2024-04-30 09:57:44 +0000

    design update

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -12,7 +12,10 @@ export const TodoForm = ({addTodo}) => {
     }
   return (
     <form className='todoForm' onSubmit={handleSubmit}>
-        <input type="text" className='todo-input' value={value} placeholder='add your item' onChange={(e) => setValue(e.target.value)}/>
+        <input type="text" className='todo-input' value={value} placeholder='add your item' onChange={(e) => {
+          if (e.target.value.length <= 35) {
+            setValue(e.target.value)
+          }}}/>
         <button type='submit' className='todo-btn'>Add item</button>
     </form>
   )
